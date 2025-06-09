1a92540dd926e661f4e769c2acc076ecb6de4a45
bluemagikSGO <stevengabriel2015@gmail.com>
2025-06-09 21:40:20 +0100

    09/06/2025

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import "./App.css";
 import HomePage from "./pages/HomePage";
 import { Route, HashRouter as Router, Routes } from "react-router-dom";
 import Metherappy from "./pages/Metherappy";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <>
       <Router>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/metherappy" element={<Metherappy />} />
