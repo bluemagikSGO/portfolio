83ac59f3b4cd845a8b811e47d74e8e50fdde1021
bluemagikSGO <stevengabriel2015@gmail.com>
2025-06-20 19:01:05 +0100

    20/06/2025

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -13,7 +13,7 @@ const Counter = () => {
      mt-15  px-25  flex flex-col items-center gap-12 text-center h-fit"
       >
         <div className="text-white text-5xl md:w-5 flex md:flex-row flex-col items-center gap-5 justify-center md:px-40 font-bold text-center">
-          <CountUp start={0} end={2} duration={3} delay={0} />
+          <CountUp start={0} end={1} duration={3} delay={0} />
           <div className="md:text-sm text-[0.9rem] font-['Montserrat']">
             Years of experience
           </div>
